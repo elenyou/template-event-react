e6f039f6bd28c050f5e1db878116c1ca12a4a870
elenyou <elenyou333@gmail.com>
2018-09-14 12:13:53 -0400

    responsive

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Footer from './components/header_footer/Footer';
 class App extends Component {
   render() {
     return (
-      <div className="App" style= { { height:"1500px"}}>
+      <div className="App">
         <Header/>
         <Element name="featured">
           <Featured/>
