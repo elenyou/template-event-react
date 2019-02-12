7f26e04672878501845781ef3a052ed8709dd5a2
elenyou <elenyou333@gmail.com>
2019-02-12 09:38:00 -0500

    fix date

--- src/components/venueInfo/index.js
+++ src/components/venueInfo/index.js
@@ -10,9 +10,9 @@ const VenueInfo = () => {
             <div className="center_wrapper">
                 <div className="vn_wrapper">
                 <Zoom duration={500}>
-                    <div className="vn_item"> 
-                        <div className="vn_outer"> 
-                            <div className="vn_inner"> 
+                    <div className="vn_item">
+                        <div className="vn_outer">
+                            <div className="vn_inner">
                                 <div className="vn_icon_square bck_red"> </div>
                                 <div className="vn_icon"
                                     style= {{
@@ -23,16 +23,16 @@ const VenueInfo = () => {
                                     Event Date & Time
                                 </div>
                                 <div className="vn_desc">
-                                    19 December 2018 @8:00pm
+                                    19 December 2019 @8:00pm
                                 </div>
                             </div>
                         </div>
                     </div>
                 </Zoom>
                 <Zoom duration={500} delay={500}>
-                   <div className="vn_item"> 
-                        <div className="vn_outer"> 
-                            <div className="vn_inner"> 
+                   <div className="vn_item">
+                        <div className="vn_outer">
+                            <div className="vn_inner">
                                 <div className="vn_icon_square bck_yellow"> </div>
                                 <div className="vn_icon"
                                     style= {{
